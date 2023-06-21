f3c19b01b69482b64e05f495e5177f3c2b6bc6ca
DanyaSantos <santosresendezde1720@gmail.com>
2023-06-20 18:37:59 -0600

    adding cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const colors = require('colors')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 const {errorHandler}= require('./middleware/errorMiddleware.js')
 const connectDB = require('./config/db.js')
@@ -7,6 +8,7 @@ const port = process.env.PORT || 5000
 connectDB()
 const app = express()
 
+app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
